fcd4d0c3ac78ff315e1548c7d61eb5e160d6c703
Saksham Singh <saksham.webdev@gmail.com>
2021-09-05 16:53:53 +0530

    website

---
+++ assets/js/custom.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function ($) {
+
+
+    "use strict";
+    
+    $(function () {
+        $("#tabs").tabs();
+    });
+
+
+
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var box = $('.header-text').height();
+        var header = $('header').height();
+
+        if (scroll >= box - header) {
+            $("header").addClass("background-header");
+        } else {
+            $("header").removeClass("background-header");
+        }
+    });
+
+
+    $("#wrapper").click(function () {
+        $(".icon").toggleClass("close");
+    });
+
+    $(window).scroll(function () {
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function () {
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    
+    $('#readmore').click(function () {
+        $('#more').slideToggle(300);
+      });
+
+});
